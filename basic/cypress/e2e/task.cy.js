8ce1ac93887d86aa92ddf7720539efc7e1ddfa1b
Achref Ghozlene <medachrefg@gmail.com>
2023-03-26 13:49:41 +0200

    practincing some examples

--- basic/cypress/e2e/task.cy.js
+++ basic/cypress/e2e/task.cy.js
@@ -3,6 +3,15 @@ describe('tasks management', () => {
 		cy.visit('http://localhost:5173/');
 		cy.contains('Add Task').click();
 		cy.get('.backdrop').click({ force: true });
+
+		cy.get('.backdrop').should('not.exist');
+		cy.get('.modal').should('not.exist');
+	});
+
+	it('should close the task model when clicking cancel', () => {
+		cy.visit('http://localhost:5173/');
+		cy.contains('Add Task').click();
+		cy.get('.modal').contains('Cancel').click();
 		cy.get('.backdrop').should('not.exist');
 		cy.get('.modal').should('not.exist');
 	});
