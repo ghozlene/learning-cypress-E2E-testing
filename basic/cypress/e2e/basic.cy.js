c363e603d320fa1b3c25eb6603eb2f18a0d84dae
Achref Ghozlene <medachrefg@gmail.com>
2023-03-26 11:38:02 +0200

    selecting elements

---
+++ basic/cypress/e2e/basic.cy.js
@@ -0,0 +1,14 @@
+// for auto-complishing you should write the following ligne  below:
+/// <reference types="Cypress" />
+
+describe('task page', () => {
+	it('should render the main image ', () => {
+		cy.visit('http://localhost:5173/');
+		cy.get('.main-header img[src="/src/assets/logo.png"]');
+	});
+
+	it('should display the page title', () => {
+		cy.visit('http://localhost:5173/');
+		cy.get('h1').contains('My Cypress Course Tasks');
+	});
+});
