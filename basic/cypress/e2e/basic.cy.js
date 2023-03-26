bf98ae5e94db94c29e3125a5fd4772760a2c1383
Achref Ghozlene <medachrefg@gmail.com>
2023-03-26 11:45:00 +0200

    Implicit and Explicit Assertions

--- basic/cypress/e2e/basic.cy.js
+++ basic/cypress/e2e/basic.cy.js
@@ -9,6 +9,7 @@ describe('task page', () => {
 
 	it('should display the page title', () => {
 		cy.visit('http://localhost:5173/');
+		cy.get('h1').should('have.length', 1);
 		cy.get('h1').contains('My Cypress Course Tasks');
 	});
 });
